ad0c8926984112674a2be8240e1fac1c37a603a9
Wanderson Duarte Alves <wandersondrtlvs.new@gmail.com>
2023-04-17 09:30:23 -0300

    criação de arquivos estáticos: css, js, img. Visualização de imagens.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,5 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', jobs.views.home, name='home'),
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    path('', jobs.views.home, name='home'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
